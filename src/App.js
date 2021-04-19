6a278e5d1d8847824e1a5121bcc2f3aaf6653494
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-04-20 00:15:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // useState change data in DOM at run time
+  const [repos, setRepos] = useState([{}]);
+
+  // useEffect is used to fetch API
+  // useEffect take two parameters 1. call back function 2. array
+  useEffect(async () => {
+
+    //  async k lye function ma function banaya aur function ko usi function ma call kya
+    // function getRepos(){
+      
+    // }
+
+    // with async awaits
+    const response = await fetch("https://api.github.com/users/hamza181/repos");
+    const data = await response.json();
+
+    console.log(data);
+    setRepos(data);;
+    console.log(repos);
+
+    // console.log(repos.name)
+
+    // fetch api without async await
+    // fetch("https://jsonplaceholder.typicode.com/posts/1")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     console.log(json);
+    //     setData(json);
+    //   });
+    // .then means it will return a promise
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Fetch API</h3>
+
+      <ul>
+        {repos.map((repoObj, ind)=>{
+          return(<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
     </div>
   );
 }
