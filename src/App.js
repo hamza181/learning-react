199f4dcab8026afd01763012125d49a735cf51fa
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-04-24 00:11:08 +0500

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, TextField, Button } from "@material-ui/core";
+import NavBar from "./NavBar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {},
+  },
+
+  paper: {
+    width: 400,
+    margin: "0 auto",
+    marginTop: 50,
+    textAlign: "center",
+    padding: 15,
+  },
+
+  input: {
+    width: "98%",
+    marginBottom: 10,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <NavBar />
+
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" label="Enter Email" className={classes.input} />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" label="Enter Password" className={classes.input} />
+          <Button variant="contained" color="primary" className={classes.input}>
+            Login
+          </Button>
+        </form>
+      </Paper>
     </div>
   );
 }
