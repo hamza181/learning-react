29812d9b23e34813c0491b4c3782b7ba00b07034
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-04-24 23:51:08 +0500

    add chart.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-// import './App.css';
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Paper, TextField, Button } from "@material-ui/core";
 import NavBar from "./NavBar";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, TextField, Button, CircularProgress } from "@material-ui/core";
+import BarChart from "./barChart";
+import PieChart from "./pieChart";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {},
-  },
-
   paper: {
     width: 400,
     margin: "0 auto",
@@ -16,9 +13,8 @@ const useStyles = makeStyles((theme) => ({
     textAlign: "center",
     padding: 15,
   },
-
   input: {
-    width: "98%",
+    width: "95%",
     marginBottom: 10,
   },
 }));
@@ -29,18 +25,20 @@ function App() {
   return (
     <div className={classes.root}>
       <NavBar />
-
       <Paper elevation={3} className={classes.paper}>
         <h1>Login</h1>
-
         <form className={classes.root} noValidate autoComplete="off">
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" label="Enter Email" className={classes.input} />
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" label="Enter Password" className={classes.input} />
+          <TextField label="Enter Email" className={classes.input}/>
+          <TextField label="Enter Password" className={classes.input}/>
           <Button variant="contained" color="primary" className={classes.input}>
             Login
           </Button>
         </form>
+        <CircularProgress value={50} />
       </Paper>
+
+      {/* <BarChart />
+      <PieChart /> */}
     </div>
   );
 }
