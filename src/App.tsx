0cd12f3d41b978e7bf572cf62369e5ce0984fad7
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-05-05 12:53:22 +0500

    create typescript app

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// Topic: Create TypeScript App
+// create react typescript app follow this link
+// https://create-react-app.dev/docs/adding-typescript
+
+// 1. TS template
+// 2. API/JOSN to bring questions
+// 3. Render each question
+// 4. User can submit questions
+// 5. Test validation/checking
+// 6. UX/UI
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
