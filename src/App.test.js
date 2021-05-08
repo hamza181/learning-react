15bb385a684db768de0dbc07a4e198755112e8c4
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-05-08 22:10:38 +0500

    write first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// this test fails
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
+});
+
+test('Find Hello', () => {
+	const { getByText } = render(<App />);
+	const linkElement = getByText(/Hello/i);
+	expect(linkElement).toBeInTheDocument();
 });
