aa88350c86c0458fcb64631c5a1d934277ea29dc
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-05-18 09:49:43 +0500

    Counter without Redux

---
+++ src/Components/App.js
@@ -0,0 +1,19 @@
+// Topic: Basic Redux
+
+import React, {useState} from 'react';
+import CounterInput from './CounterInput';
+import CounterOutput from './CounterOutput';
+
+function App() {
+    const [counter, setCounter] = useState(0);
+
+    return(
+        <div className='App'>
+            <CounterOutput counter={counter}/>
+            <br></br>
+            <CounterInput setCounter = {setCounter}counter={counter}/>
+        </div>
+    )
+}
+
+export default App;
