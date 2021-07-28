6c38103aaaa5882e875328aab449aeb2ffffaa6a
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-07-28 14:43:00 +0500

    update index.js

--- src/store.js
+++ src/store.js
@@ -3,6 +3,10 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    // chrome extension redux devtool ki waja se kya
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
 export default store;
