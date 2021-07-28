6a5d960af9b0cfdae9b0bab1b1475196e2d3fccb
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-07-28 14:08:01 +0500

    added ui

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Redux</h1>
+      <h2>Increament Decreament Counter</h2>
+
+      <button> - </button>
+      {0}
+      <button> + </button>
+      
     </div>
   );
 }
