7b64be2b3724ba0e79087d314dfe1a1ba4fb5f80
Ameer Hamza <ameer.hamza1811@gmail.com>
2022-02-18 02:24:38 +0500

    should component update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import ClassComponent from './components/ClassComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import ClassComponent from "./components/ClassComponent";
+import { useState } from "react";
 
 function App() {
+  const [age, setAge] = useState(20);
+
+  const onClick = () => {
+    setAge(age + 1);
+  };
   return (
     <div className="">
-      <ClassComponent />
+      <ClassComponent age={age} />
+      <button onClick={onClick}>Change Age</button>
     </div>
   );
 }
--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -10,7 +10,7 @@ export default class ClassComponent extends Component {
   }
 
   onClick() {
-    this.setState({ name: "Hamza" });
+    this.setState({ id: this.state.id + 1 });
   }
 
   //   it runs when the component is mounted to the DOM
@@ -25,6 +25,29 @@ export default class ClassComponent extends Component {
     console.log("componentDidUpdate");
   }
 
+  // componentDidUpdate chalna chahiye ya nahi is lye shouldComponentUpdate use karte hen
+  //   shouldComponentUpdate() {
+  //     // if return false then component will not update
+  //     // if return true then component will update
+  //     return true;
+  //   }
+
+  //   if we want to call componentdidupdate on specific state or props changes
+  shouldComponentUpdate(nextProps, nextState) {
+    //   jab state name change hogi to componentdidupdate chale ga and then component rerender hoga
+    if (nextState.name !== this.state.name) {
+      return true;
+    }
+    // jab props.age change hogi to componentdidupdate chale ga and then component rerender hoga
+    if (nextProps.age !== this.props.age) {
+      return true;
+    }
+
+    // is k elawa koi aur state ya props change hoga to component rerender nahi hoga kyn k componentdidupdate nahi chale ga
+    // else
+    return false;
+  }
+
   // it runs when the component is unmounted from the DOM
   // it runs when component is destroyed or removing from the DOM or change the route
   componentWillUnmount() {
@@ -35,9 +58,9 @@ export default class ClassComponent extends Component {
     return (
       <div>
         <p>
-          Name: {this.state.name} Id: {this.state.id}
+          Name: {this.state.name} Id: {this.state.id} Age: {this.props.age}
         </p>
-        <button onClick={this.onClick.bind(this)}>Click</button>
+        <button onClick={this.onClick.bind(this)}>Change Id</button>
       </div>
     );
   }
